5bbed5ba4939658341471b466f8fc225b60a03a7
Will Salisbury <wjs@uber.com>
2016-12-29 18:35:21 -0500

    lifetimes are hard…

--- src/main.rs
+++ src/main.rs
@@ -8,51 +8,95 @@ use rand::random;
 type DocId = u64;
 
 // Index
-struct InvertedIndex<'x> {
+struct InvertedIndex {
     // Token --> [ DocId ]
-    index: HashMap<&'x str, Vec<DocId>>,
+    index: HashMap<String, Vec<DocId>>,
+
+    // DocId --> Body
+    docs: HashMap<DocId, String>,
 }
 
-impl<'x> InvertedIndex<'x> {
-    fn new() -> InvertedIndex<'x> {
-        return InvertedIndex { index: HashMap::new() };
+impl InvertedIndex {
+    fn new() -> InvertedIndex {
+        return InvertedIndex {
+            index: HashMap::new(),
+            docs: HashMap::new(),
+        };
     }
 
-    fn tokenize(&self, payload: &'x str) -> Vec<&'x str> {
-        return payload.split_whitespace().collect();
-    }
-
-    fn index(&mut self, payload: &'x str) -> DocId {
-        let id: DocId = random();
-        let tokens = self.tokenize(payload);
+    fn tokenize(&self, payload: &str) -> Vec<String> {
+        let lower = payload.to_lowercase();
+        let tokens: Vec<_> = lower.split_whitespace().collect();
+        let mut ret = Vec::new();
         for token in tokens {
-            self.index.entry(token).or_insert(vec![]).push(id);
+            ret.push(token.to_string());
+        }
+        ret
+    }
+
+    fn index(&mut self, payload: String) -> DocId {
+        // Generate document id
+        let id: DocId = random();
+
+        // Tokenize document
+        let tokens = self.tokenize(&payload);
+
+        // Store whole document
+        self.docs.insert(id, payload);
+
+        // Index document by tokens
+        for token in tokens {
+            self.index
+                .entry(token)
+                .or_insert(vec![])
+                .push(id);
         }
         id
     }
 
-    fn search(&self, query: &str) -> HashSet<DocId> {
-        let mut docs: HashSet<DocId> = HashSet::new();
-        let tokens = self.tokenize(query);
-        for token in tokens {
+    fn search(&self, query: String) -> HashSet<DocId> {
+        let mut docs = HashSet::new();
+        let tokens = self.tokenize(&query);
+        for token in &tokens {
             match self.index.get(token) {
                 Some(ids) => {
                     for id in ids {
                         docs.insert(*id);
                     }
                 }
-                None => {}
+                _ => {}
             }
         }
         docs
+    }
+
+    fn lookup(&self, doc_id: DocId) -> Option<&String> {
+        self.docs.get(&doc_id)
+        // match self.docs.get(&doc_id) {
+        //     Some(doc) => println!("{}: {}", doc_id, doc),
+        //     None => println!("None"),
+        // }
     }
 }
 
 
 fn main() {
+    // Create and populate documents
     let mut idx = InvertedIndex::new();
-    idx.index("The quick brown fox jumps over the lazy dog");
-    idx.index("the world is flat");
-    println!("{:?}", idx.search("the"));
-    println!("{:?}", idx.search("foo"));
+    idx.index("The quick brown fox jumps over the lazy dog".to_string());
+    idx.index("life, universe, and everything".to_string());
+    idx.index("The world is flat".to_string());
+
+    // Search for a term
+    let query = "what the";
+    let doc_ids = idx.search(query.to_string());
+
+    // Display found documents
+    for doc_id in &doc_ids {
+        let doc = idx.lookup(*doc_id);
+        println!("Query({}) -> DocId({}) -> Document({:?})",
+                 query,
+                 doc_id,
+                 doc);
+    }
 }
